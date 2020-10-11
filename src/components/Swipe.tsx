1b113a7fa77ed8dbe92cca5dfac0ba0e5ce67206
iinus <iinusen7@gmail.com>
2020-10-11 15:43:09 +0200

    add Animation and pan responder

--- src/components/Swipe.tsx
+++ src/components/Swipe.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, Text, StyleSheet, PanResponder, Animated } from "react-native";
 
 const styles = StyleSheet.create({
   detailWrapper: {
@@ -31,14 +31,35 @@ interface ISwipeProps {
 }
 
 const Swipe = ({ data }: ISwipeProps) => {
+  const position = useRef(new Animated.ValueXY()).current;
+  const _panResponder = PanResponder.create({
+    // Ask to be the responder:
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: position.x, // x,y are Animated.Value
+        dy: position.y,
+      },
+    ]),
+    onPanResponderRelease: () => {
+      // The user has released all touches while this view is the
+      // responder. This typically means a gesture has succeeded
+    },
+  });
+
   const renderCardItem = (card: any, index: number) => {
     return (
-      <View key={index} style={styles.cardStyle}>
+      <Animated.View
+        key={index}
+        {..._panResponder.panHandlers}
+        style={[position.getLayout(), styles.cardStyle]}
+      >
         <View style={styles.detailWrapper}>
           <Text style={styles.cardType}>{card.type}</Text>
           <Text style={styles.cardQuestion}>{card.question}</Text>
         </View>
-      </View>
+      </Animated.View>
     );
   };
 
