efd304a2bfb3218c33cc5d6b1136c0bafbe16ee0
iinus <iinusen7@gmail.com>
2020-10-11 14:41:35 +0200

    add data test file

---
+++ src/data/data.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    type: "Dilemma",
+    question: "Paradise hotel eller Love Island",
+  },
+  {
+    type: "Jeg har aldri",
+    question: "Sovet over på skolen",
+  },
+];
