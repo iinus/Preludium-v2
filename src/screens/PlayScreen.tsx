d9329f0e6e3cee585dde13c2c326bb8feb6b5079
iinus <iinusen7@gmail.com>
2020-10-11 14:19:27 +0200

    add logos

--- src/screens/PlayScreen.tsx
+++ src/screens/PlayScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { NavigationStackProp } from "react-navigation-stack";
 
 const styles = StyleSheet.create({
