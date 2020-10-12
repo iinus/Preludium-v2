b5a75ca52df3982f2a819502df1b847664c27f33
iinus <iinusen7@gmail.com>
2020-10-12 21:19:45 +0200

    add some styling to choose packets screen

--- src/screens/PlayScreen.tsx
+++ src/screens/PlayScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Swipe from "../components/Swipe";
 import data from "../data/data";
 
