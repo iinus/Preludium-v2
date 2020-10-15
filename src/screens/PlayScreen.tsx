203007496cc4cf365f8e3431f4934fded0f3950d
iinus <iinusen7@gmail.com>
2020-10-15 17:59:14 +0200

    add logo as well

--- src/screens/PlayScreen.tsx
+++ src/screens/PlayScreen.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import Swipe from "../components/Swipe";
 import data from "../data/data";
 import BackGroundSwirls from "../assets/BackgroundSwirls.svg";
+import Logo from "../assets/Logo.svg";
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   background: {
@@ -16,11 +20,17 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
+  logo: {
+    position: "absolute",
+    top: SCREEN_HEIGHT * 0.08,
+    left: SCREEN_WIDTH * 0.296,
+  },
 });
 
 const PlayScreen = () => {
   return (
     <View style={styles.playscreen}>
+      <Logo width={152} style={styles.logo} />
       <BackGroundSwirls style={styles.background} />
       <Swipe data={data}></Swipe>
     </View>
