e5e928651ea1935bd2255fdc0629f1afc4e0d6c0
iinus <iinusen7@gmail.com>
2020-10-15 19:23:45 +0200

    add help icon

--- src/screens/PlayScreen.tsx
+++ src/screens/PlayScreen.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
 import Swipe from "../components/Swipe";
 import data from "../data/data";
 import BackGroundSwirls from "../assets/BackgroundSwirls.svg";
 import Logo from "../assets/Logo.svg";
+import Help from "../assets/Help.svg";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 const SCREEN_WIDTH = Dimensions.get("window").width;
@@ -20,17 +22,29 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
-  logo: {
-    position: "absolute",
-    top: SCREEN_HEIGHT * 0.08,
-    left: SCREEN_WIDTH * 0.296,
+  logo: {},
+  help: {
+    marginTop: 10,
+  },
+  topWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-around",
+    marginLeft: SCREEN_WIDTH * 0.21,
+    marginTop: SCREEN_HEIGHT * 0.08,
   },
 });
 
 const PlayScreen = () => {
   return (
     <View style={styles.playscreen}>
-      <Logo width={152} style={styles.logo} />
+      <View style={styles.topWrapper}>
+        <Logo width={152} style={styles.logo} />
+        <TouchableOpacity onPress={() => console.log("hello")}>
+          <Help width={24} height={24} style={styles.help} />
+        </TouchableOpacity>
+      </View>
       <BackGroundSwirls style={styles.background} />
       <Swipe data={data}></Swipe>
     </View>
